567a0b08fabf17210a365bd64ceb3fb0cd550aa0
Nadine Ju <nadine.jura@online.de>
2019-10-08 13:34:58 -0700

    jQuery: declared variables, defined function for random number, displayed random number, wins, and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,35 @@
+/*******************************************
+VARIABLES
+*******************************************/
+var totalScore = 0;
+var wins = 0;
+var losses = 0;
+var randomNum = 0;
+
+/*******************************************
+FUNCTIONS
+*******************************************/
+
+function getRandomInt(min, max) {
+    /**
+    * Returns a random integer between min (inclusive) and max (inclusive).
+    **/
+    var min = Math.ceil(19);
+    var max = Math.floor(120);
+    return randomNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+}
+
 $(document).ready(function () {
+    // display random Number chosen by computer
+    getRandomInt();
+    $("#random").text(randomNum);
+
+    // display wins and losses
+    $("#wins").text(wins);
+    $("#losses").text(losses);
+
+
+
 
 
 });
